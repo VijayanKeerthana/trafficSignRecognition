8dcc8b1d4e95f229ff018f5526c0bfb9a78fd8cb
Devika <devika.ajith@next-prototypes.de>
2022-01-15 13:26:04 +0100

    feat/impl vid detec and classification(incorrectly though)

--- hw_impl/yolo_video_detection.py
+++ hw_impl/yolo_video_detection.py
@@ -17,154 +17,6 @@ import time
 classes = ['prohibitory', 'danger', 'mandatory', 'other']
 detected_img = 0
 
-"""
-    Captures traffic sign from a video frame, crops it and writes it to a folder
-"""
-def yolo_detect_sign(img_path):
-
-    net = cv2.dnn.readNet("yolov4-tiny_training_1000.weights", "yolov4-tiny_training.cfg")
-    layer_names = net.getLayerNames()
-    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
-    colors = np.random.uniform(0, 255, size=(len(classes), 3))
-    img = cv2.imread(img_path)
-    img = cv2.resize(img, None, fx=0.4, fy=0.4)
-    height, width, channels = img.shape
-    
-    # Object detection
-    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-    net.setInput(blob)
-    outs = net.forward(output_layers)
-    class_ids = []
-    confidences = []
-    boxes = []
-    for out in outs:
-        for detection in out:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            if confidence > 0.1:
-                center_x = int(detection[0] * width)
-                
-                center_y = int(detection[1] * height)
-                w = int(detection[2] * width)
-                h = int(detection[3] * height)
-                x = int(center_x - w / 2)
-                y = int(center_y - h / 2)
-                boxes.append([x, y, w, h])
-                confidences.append(float(confidence))
-                class_ids.append(class_id)
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-    font = cv2.FONT_HERSHEY_PLAIN
-    scale = 1
-    fontScale = min(416,416)/(25/scale)
-    for i in range(len(boxes)):
-        if i in indexes:
-            x, y, w, h = boxes[i]
-            label = str(classes[class_ids[i]])
-            color = colors[class_ids[i]]
-            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-            
-        
-            cv2.putText(img, label, (x, y + 30), font, 3, color, 1)
-        roi = img[y:y+h, x:x+w]
-        tbd_image_path = "test_images_cnn/00004.png"
-        output_path = "classified_images_cnn/classified%d.jpg" %i
-        cv2.imwrite("detected_images_yolo/detected%d.jpg" %i, roi)
-        model_name = 'trafficsignnet2.model'
-        prediction(model_name, tbd_image_path, output_path)
-    cv2.imshow("Image", img)
-    
-    key = cv2.waitKey(0)
-
-    cv2.destroyAllWindows()
-    
-    
-""" 
-    Classifies the image and sounds a buzzer
-"""
-def crop_box(image,line):
-    
-    #split the line
-    box = [float(i) for i in line.split()]
-    h, w = image.shape[:2]
-    
-    #need to multiply with width/height since bounding boxes are in percentage
-    x_center, y_center = (box[1] * w), (box[2] * h)
-    box_width, box_height = (box[3] * w), (box[4] * h)
-    x_min, y_min = (x_center - box_width/2), (y_center - box_height/2)
-    
-    #cropping region of interest
-    roi = image[int(y_min):int(y_min+box_height), int(x_min):int(x_min+box_width)]
-    
-    return roi, [x_min, y_min,box_width, box_height]
-#function to predict label for each input box
-
-def predict_label(model, roi):
-        
-    # load the label names
-    labelNames = open("signnames.csv").read().strip().split("\n")[1:]
-    labelNames = [l.split(",")[1] for l in labelNames]
-    
-    #predict
-    image = cv2.resize(roi, (32, 32))
-    #image = exposure.equalize_adapthist(image, clip_limit=0.1)
-    
-    # preprocess the image by scaling it to the range [0, 1]
-    image = image.astype("float32") / 255.0
-    image = np.expand_dims(image, axis=0)
-    
-    # make predictions
-    preds = model.predict(image)
-    j = preds.argmax(axis=1)[0]
-    label = labelNames[j]
-    
-    return label
-#function to draw boxes and labels on the first input image
-
-def detected_image(image_path, bbs, labels):
-    image = imageio.imread(image_path)
-    for box,label in zip(bbs,labels):
-        cv2.rectangle(image, (int(box[0]), int(box[1])), (int(box[0] + box[2]), int(box[1] + box[3])), [172 , 10, 127], 2)
-        cv2.putText(image, label, (int(box[0] + box[2]), int(box[1] + box[3])), cv2.FONT_HERSHEY_SIMPLEX,0.45, (0, 0, 255), 2)
-        
-    return image
-def plot_img(image, label,size=[6.4,4.8]):
-    plt.figure(figsize=size)
-    #plt.axis(False)
-    plt.title(label)
-    plt.imshow(image)
-    return
-def prediction(model_name, tbd_image_path,detected_output_path):
-    print("[INFO] loading model...")
-    model = load_model(model_name)
-    
-    print("[INFO] predicting...")
-    input_image = imageio.imread(tbd_image_path)
-
-    plot_img(input_image, tbd_image_path.split("\\")[-1],[12,9])
-
-    #since YOLO dataset has similar naming structure
-    txt_path = tbd_image_path[:-3]+"txt"
-    boxes=[]
-    labels = []
-    with open (txt_path, "r+") as f:
-        for line in f:
-            roi, box = crop_box(input_image,line)
-            boxes.append(box)
-
-            label = predict_label(model, roi)
-            labels.append(label)
-
-            plot_img(roi, label,[3,3])
-        
-    image = detected_image(tbd_image_path, boxes, labels)    
-    plot_img(image, "final", [16,12])
-    
-    #save in an output folder
-    imageio.imsave(detected_output_path,image)
-   # detected_img = detected_img + 1
-    return
-
 """ 
     Displays detected and classified image from a video frame
 """
@@ -192,10 +44,10 @@ if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
     
     '''Load Classification Model'''
-#    classification_model = load_model('traffic.h5') #load mask detection model
-#    classes_classification = []
-#    with open("signnames.csv", "r") as f:
-#        classes_classification = [line.strip() for line in f.readlines()]
+    classification_model = load_model('trafficsignnet2.model') #load mask detection model
+    classes_classification = []
+    with open("signnames.csv", "r") as f:
+        classes_classification = [line.strip() for line in f.readlines()]
     
     tfps = 30
     cap = cv2.VideoCapture('video_data/3.mp4')
@@ -205,14 +57,65 @@ if __name__ == '__main__':
     while(True):
         ret, frame = cap.read()
         if not ret:
-            print('nooo')
+#            print('nooo')
             break
-        if curr_frame % hop == 0:
-            print('capturing frames')
-            path = 'captured_frames/frame'+str(curr_frame)+'.jpg'
-            cv2.imwrite("captured_frames/frame%d.jpg" %curr_frame, frame)
-            curr_frame += 1
-            
-    cap.release()
-    #yolo_detect_sign(path)
+#        if curr_frame % hop == 0:
+        print('capturing frames')
+        path = 'captured_frames/frame'+str(curr_frame)+'.jpg'
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        #get image shape
+        frame_count += 1
+        height, width, channels = img.shape
+        window_width = width
+        
+        #detecting objects (YOLO)
+        blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+        net.setInput(blob)
+        outs = net.forward(output_layers)
+        
+        # Show bounding box on the screen
+        class_ids = []
+        confidences = []
+        boxes = []
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > confidence_threshold:
+                    # Object detected
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+                    # Rectangle coordinates
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+                    boxes.append([x, y, w, h])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+        for i in range(len(boxes)):
+            if i in indexes:
+                x, y, w, h = boxes[i]
+                label = str(classes[class_ids[i]]) + "=" + str(round(confidences[i]*100, 2)) + "%"
+                cv2.rectangle(img, (x, y), (x + w, y + h), (255,0,0), 2)
+                '''crop the detected signs -> input to the classification model'''
+                crop_img = img[y:y+h, x:x+w]
+                if len(crop_img) >0:
+                    crop_img = cv2.resize(crop_img, (WIDTH, HEIGHT))
+                    crop_img =  crop_img.reshape(-1, WIDTH,HEIGHT,3)
+                    prediction = np.argmax(classification_model.predict(crop_img))
+                    label = str(classes_classification[prediction])
+                    cv2.putText(img, label, (x, y), font, 0.5, (255,0,0), 2)
 
+        elapsed_time = time.time() - start_time
+        fps = frame_count/elapsed_time
+        print ("fps: ", str(round(fps, 2)))
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) & 0xFF == ord ('q'):
+            break
+    cv2.destroyAllWindows()
+        
+        
